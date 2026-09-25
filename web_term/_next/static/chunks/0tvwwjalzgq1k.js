46cd973107da57970d809b798e0d130ac19ce30c
Paul Robello <probello@gmail.com>
2026-09-24 20:26:21 -0700

    build(web): regenerate web_term/ static export for the viewer size policy

--- web_term/_next/static/chunks/0tvwwjalzgq1k.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,36557,o=>{o.v(t=>Promise.all(["static/chunks/123awczwreopd.js"].map(t=>o.l(t))).then(()=>t(7074)))}]);
